182e8b436a1fc7eb75302ccbb3f36c8a760ae74b
mrchinnu <ganesh_mcr@srkrec.edu.in>
2024-10-04 02:22:40 -0400

    add basic html css js required add basic js functs
    added basic html and css to make it look like a calculator
    
    added basic js functions for basic operations

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator
+basic calculator made with html css and js.
---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+    return a+b;
+}
+
+function substract(a,b){
+    return a-b;
+}
+
+function multiplication(a,b){
+    return a*b;
+}
+
+function division(a,b){
+    return a/b;
+}
+
+let firstnumber;
+let secondnumber;
+let operator;
+
+
+function opreation(first,operator,second){
+
+}
+
+const buttons=document.querySelector(".buttons");
+const display=document.querySelector(".display");
+
+
